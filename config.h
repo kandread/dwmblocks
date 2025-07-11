26b1cce92473778348c7e30a0bf3785726dbc08f
Kostas Andreadis <kandread@umass.edu>
2025-07-11 11:22:45 -0400

    Update config.h

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+{"", "/home/kandread/.local/bin/sb-volume", 0, 10},
         {"", "date '+%a %b %d %I:%M%p'",					60,		0}
 };
 
